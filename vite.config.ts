8cf4418ce294ce1c095a9446b6aa09374f65e5ed
keita yamada <keita3114@gmail.com>
2025-02-28 11:52:40 +0900

    Update app with pinch-zoom and scroll position fixes

--- vite.config.ts
+++ vite.config.ts
@@ -32,13 +32,13 @@ export default defineConfig({
         theme_color: '#4A90E2',
         icons: [
           {
-            src: '/icons/icon-192x192.png',
+            src: './icons/icon-192x192.png', // ./で相対パスに変更
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: '/icons/icon-512x512.png',
+            src: './icons/icon-512x512.png', // ./で相対パスに変更
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
